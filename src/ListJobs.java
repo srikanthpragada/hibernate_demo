020bf2381d452ba9a76cba447de5eef9a72b6cc5
srikanthpragada <srikanthpragada@gmail.com>
2016-04-12 09:34:45 +0530

    List

---
+++ src/ListJobs.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.Job;
+
+public class ListJobs {
+
+	public static void main(String[] args) throws Exception {
+     	Configuration c  = new Configuration();
+		c.configure(); // loads hibernate.cfg.xml
+		
+		SessionFactory sf = c.buildSessionFactory();
+		Session s = sf.openSession();
+		Query q = s.createQuery("from Job");
+		
+		for (Job j : (List<Job>) q.list())
+		{
+			System.out.println(j.getTitle());
+		}
+		
+		
+		
+		s.close();
+		sf.close();
+	}
+
+}
